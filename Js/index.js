fc2eb89af9e9be66308cf15b44e28d03a0fe94cd
BalaVinay18 <vinayvinu0046@gmail.com>
2024-03-25 13:00:06 +0530

    ii

---
+++ Js/index.js
@@ -0,0 +1,42 @@
+console.log("hello Ashok ,Harshith oka  by blessto");
+console.log(1+5);
+const pi=3.1415;
+
+var x=1;
+console.log(x);
+
+var x =1;
+console.log(x);
+var y=2;
+console.log(y);
+var z ="hello";
+console.log(z);
+var a =[x,y,z];
+console.log(a);
+var lst = [1,"0",[1,2],4,8,true];
+console.log(lst);
+var obj = {
+    name:"Mr Ashok",
+    job:"Travelling from housr to banglore",
+    age:19,
+    addres: "hosur",
+}
+console.log(obj);
+
+let outcome = [
+                [1,2,3,4], 
+                [2,3,1,4], 
+                [3,4,2,1],
+                [1,4,3,2], 
+                [4,3,2,1], 
+                [4,1,2,3]]
+console.log(outcome[1][3]);
+console.log(outcome[5][2]);
+
+let d3 = [
+            [[1,2], [1,3]],
+            [[2,1],[3,1]],
+            [[1,3],[2,2]],
+            [[2,3],[3,1]]
+]
+console.log(d3[1][2]);
